0800c66d4827a72f96b9156b3f3919862e05e15d
satyagyasingh <satyagyasingh@gmail.com>
2024-09-12 22:41:53 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import KeyInitiatives from "./components/KeyInitiatives";
 
 export default function Home() {
   return (
     <div>
-      Homepage
+      <KeyInitiatives />
     </div>
   );
 }
